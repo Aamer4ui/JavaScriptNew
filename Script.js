87f913ff7365e8f93e9056ca8b3f6b3ad154a2ef
Aamer4ui <aamersohelansari@gmail.com>
2021-03-01 15:50:26 +0530

    practice

---
+++ Script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+//Merging Strings Togethrer
+
+let a = "Hello there!"
+alert(a + " " + "Have A Nice Day"); 
+
+//Promt box-  prompt for values from the user
+//Always assign it to a variable
+
+let age = prompt("Whats is your age?");
+console.log (age);
+alert("Your Age is:" +  age); // First Step
+// alert(`Your age is ${age}`); //You overwrite Can write this type of step to giving outputs
+
+// ---------------Confirm Box------------
+
+//used to yes or no answers like true or false
+
+// let age = confirm("Are You less than 5 years olds?");
+// console.log(age); //If statements further calculations etc
